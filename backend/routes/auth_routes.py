d018a634abcedebb051f2b7a99c5a9166aa429cc
ompratapsinghparihar <ompratapsinghparihar0@gmail.com>
2026-04-25 01:37:38 -0700

    fixed axios await error

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -15,10 +15,13 @@ def get_db():
 
 @router.post("/login", response_model=schemas.Token)
 def login(data: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == data.email).first()
+    user = db.query(User).filter(User.email == data.email).first()
 
-    if not user or not verify_password(data.password, user.password):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+    if not user:
+       raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    if not verify_password(data.password, user.password):
+       raise HTTPException(status_code=401, detail="Invalid credentials")
 
     token = create_access_token({"user_id": user.id})
     return {"access_token": token, "token_type": "bearer"}
