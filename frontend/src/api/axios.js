1c290be02666b645f54cd9582bdf4d1752d4a89e
ompratapsinghparihar <ompratapsinghparihar0@gmail.com>
2026-04-25 01:42:09 -0700

    fixed http to https final

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://inventory-tracker-production-f795.up.railway.app/",
+  baseURL: "https://inventory-tracker-production-f795.up.railway.app",
 });
 
-// Request interceptor (auto token attach karega)
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token"); // ❌ NO await
+    const token = localStorage.getItem("token");
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -15,9 +14,7 @@ instance.interceptors.request.use(
 
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default instance;
